80fb635572aaa952666a824c1fa2f0159832e24a
Matthew Cheely <matthew.cheely@inin.com>
2017-03-06 13:43:18 -0500

    Change client create/connect API for more flexible integrations.

--- src/client.js
+++ src/client.js
@@ -6,54 +6,75 @@ let extensions = {
   notifications: notifications
 };
 
-function connection (client) {
+function mergeOptions(destination, provided) {
+  for ( var key in provided ) {
+    let value = provided[key]
+    if (typeof value === "object" ) {
+      if (! destination[key]) {
+        destination[key] = {}
+      }
+      mergeOptions(destination[key], value);
+    } else {
+      destination[key] = provided[key];
+    }
+  }
+
+  return destination;
+}
+
+function stanzaioOptions(pcOptions) {
+  let wsHost = pcOptions.host.replace(/\/$/, '');
+  let stanzaOptions = {
+    jid: pcOptions.jid,
+    credentials: {
+      username: pcOptions.jid,
+      password: `authKey:${pcOptions.authToken}`
+    },
+    wsURL: `${wsHost}/stream`,
+    transport: 'websocket',
+  };
+
+  return stanzaOptions;
+}
+
+function client(clientOptions) {
+  let stanzaioOpts = stanzaioOptions(clientOptions);
+  let stanzaClient = XMPP.createClient(stanzaioOpts)
   let subscribedTopics = [];
 
-  let connection = {
+  let client = {
     connected: false,
     subscribedTopics: subscribedTopics,
-    on: client.on.bind(client),
-    connect: client.connect.bind(client),
-    disconnect: client.disconnect.bind(client)
+    on: stanzaClient.on.bind(stanzaClient),
+    disconnect: stanzaClient.disconnect.bind(stanzaClient),
+    connect (connectionOptions) {
+      let options = mergeOptions(clientOptions, connectionOptions);
+      stanzaClient.connect(stanzaioOptions(options));
+    }
   };
 
   client.on('connected', function () {
-    connection.connected = true;
+    client.connected = true;
   });
 
   client.on('disconnected', function () {
-    connection.connected = false;
+    client.connected = false;
   });
 
-  connection.on('session:started', function (event) {
-    connection.streamId = event.resource;
+  client.on('session:started', function (event) {
+    client.streamId = event.resource;
   });
 
   Object.keys(extensions).forEach((extensionName) => {
-    connection[extensionName] = extensions[extensionName](client);
+    client[extensionName] = extensions[extensionName](stanzaClient);
   });
 
-  return connection;
+  return client;
 }
 
 module.exports = {
 
-  connection (host, jid, auth) {
-    let wsHost = host.replace(/http:\/\//, 'ws://')
-                         .replace(/https:\/\//, 'wss://');
-    let wsUrl = `${wsHost}/stream`;
-    var client = XMPP.createClient({
-      jid: jid,
-      credentials: {
-        username: jid,
-        password: auth
-      },
-      transport: 'websocket',
-      wsURL: wsUrl
-    });
-
-    return connection(client);
-  },
+  client: client,
 
   extend (namespace, extender) {
     if (extensions[namespace]) {
--- test/unit/client.test.js
+++ test/unit/client.test.js
@@ -3,43 +3,31 @@
 const test = require('tap').test;
 const td = require('../helpers').td;
 
-let client, xmppInfo, XMPP, extendObject, clientStanza;
+let client, xmppInfo, extendObject, clientStanza, stanzaioInstance;
 test('setup', t => {
-  xmppInfo = [
-    'anon@anon.lance.im',
-    'websocket',
-    'wss://lance.im/xmpp-websocket'
-  ];
-  XMPP = {
-    createClient: () => {
-      return xmppInfo;
-    }
+  xmppInfo = {
+    jid: 'anon@anon.lance.im',
+    authToken: 'AuthToken',
+    host: 'wss://example.com/test'
   };
+
+  //Stub stanzaio connection
+  stanzaioInstance = {
+    on: td.function('.on'),
+    connect: td.function('.connect'),
+    disconnect: td.function('.disconnect'),
+  };
+
+  //Stup stanzaio library
   clientStanza = {
-    on: (str, cb) => {},
-    disconnect: () => {},
-    subscribedTopics: [],
-    connected: false,
-    createClient: () => {
-      return {
-        on: {
-          bind: (client) => client
-        },
-        on: () => {},
-        connect: () => {
-          return {
-            bind: (client) => client
-          };
-        },
-        disconnect: () => {
-          return {
-            bind: (client) => client
-          };
-        }
-      };
-    }
+    createClient: td.function('.createClient')
   };
+
+  td.when(clientStanza.createClient(td.matchers.anything()))
+    .thenReturn(stanzaioInstance);
+
   td.replace('stanza.io', clientStanza);
+
   client = require('../../src/client.js');
   t.end();
 });
@@ -49,9 +37,37 @@ test('client should return a module', t => {
   t.ok(client, 'should return client stub module');
 });
 
-test('connect event', t => {
-  t.plan(1);
-  t.ok(client.connection(...XMPP.createClient()));
+test('client creation', t => {
+  client.client(xmppInfo);
+  td.verify(clientStanza.createClient({
+      jid: 'anon@anon.lance.im',
+      credentials: {
+        username: 'anon@anon.lance.im',
+        password: 'authKey:AuthToken'
+      },
+      transport: 'websocket',
+      wsURL: 'wss://example.com/test/stream'
+  }));
+  t.end();
+});
+
+test('connect jid override', t => {
+  let con = client.client(xmppInfo);
+  con.connect({
+    jid: 'anon2@anon.lance.im',
+  });
+
+  td.verify(stanzaioInstance.connect({
+      jid: 'anon2@anon.lance.im',
+      credentials: {
+        username: 'anon2@anon.lance.im',
+        password: 'authKey:AuthToken'
+      },
+      transport: 'websocket',
+      wsURL: 'wss://example.com/test/stream'
+  }));
+
+  t.end();
 });
 
 test('extend should return an extendObject', t => {
