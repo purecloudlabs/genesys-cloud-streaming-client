22c51de2dcd901b0bdd8e4b1f995442594b4c8ee
Brian Dupuis <brian.dupuis@genesys.com>
2017-06-13 14:43:36 -0400

    Checkpoint before refactor.

--- src/client.js
+++ src/client.js
@@ -23,6 +23,8 @@ function mergeOptions(destination, provided) {
 }
 
 function stanzaioOptions(pcOptions) {
+  const DEFAULT_PING_INTERVAL = 20000;
+  const DEFAULT_MAXIMUM_FAILED_PINGS_BEFORE_DISCONNECT = 1;
   let wsHost = pcOptions.host.replace(/\/$/, '');
   let stanzaOptions = {
     jid: pcOptions.jid,
@@ -32,6 +34,10 @@ function stanzaioOptions(pcOptions) {
     },
     wsURL: `${wsHost}/stream`,
     transport: 'websocket',
+    pingInterval: pcOptions.pingInterval === undefined ? DEFAULT_PING_INTERVAL : pcOptions.pingInterval, 
+    failedPingsBeforeDisconnect: pcOptions.failedPingsBeforeDisconnect === undefined 
+      ? DEFAULT_MAXIMUM_FAILED_PINGS_BEFORE_DISCONNECT 
+      : pcOptions.failedPingsBeforeDisconnect
   };
 
   return stanzaOptions;
@@ -41,6 +47,37 @@ function client(clientOptions) {
   let stanzaioOpts = stanzaioOptions(clientOptions);
   let stanzaClient = XMPP.createClient(stanzaioOpts)
   let subscribedTopics = [];
+
+  let xmppPingClient = {
+    pingIntervalId: null,
+    numberOfFailedPings: 0,
+    start() {
+      if(xmppPingClient.pingIntervalId === null) {
+        xmppPingClient.pingIntervalId = setInterval(xmppPingClient.performPing, stanzaioOpts.pingInterval);
+      }
+    },
+    performPing() {
+      stanzaClient.ping(stanzaioOpts.jid, xmppPingClient.pingCallback);
+    },
+    stop() {
+      if(xmppPingClient.pingIntervalId !== null) {
+        clearInterval(xmppPingClient.pingIntervalId);
+        xmppPingClient.pingIntervalId = null;
+      }
+    },
+    pingCallback(error, response) {
+      if(response && !error) {
+        console.debug("Ping received");
+        xmppPingClient.numberOfFailedPings = 0;
+      } else if(error) {
+        console.warn("Missed a ping.")
+        if(++xmppPingClient.numberOfFailedPings > stanzaioOpts.failedPingsBeforeDisconnect) {
+          console.error("Missed "+xmppPingClient.numberOfFailedPings+" pings, disconnecting");
+          stanzaClient.sendStreamError("too many missed pongs");
+        }
+      }
+    }
+  };
 
   let client = {
     connected: false,
@@ -59,10 +96,16 @@ function client(clientOptions) {
 
   client.on('disconnected', function () {
     client.connected = false;
+    xmppPingClient.stop();
   });
 
   client.on('session:started', function (event) {
     client.streamId = event.resource;
+    xmppPingClient.start();
+  });
+
+  client.on('session:end', function (event) {
+    xmppPingClient.stop();
   });
 
   Object.keys(extensions).forEach((extensionName) => {
