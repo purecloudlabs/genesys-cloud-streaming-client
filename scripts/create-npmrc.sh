f304f86308b2cf203f6c19fea902fd239f8356d0
Xander Dumaine <xander.dumaine@genesys.com>
2017-11-20 10:28:29 -0500

    Revert "Revert "tmp beta branch to publish beta version""
    This reverts commit 3a16b1bc795f5376b9f1a5570cd0497ebb843bca.

--- scripts/create-npmrc.sh
+++ scripts/create-npmrc.sh
@@ -2,4 +2,6 @@
 
 printf "Creating npmrc file\n"
 
-printf "registry=https://purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:_password=%s\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:username=%s\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:always-auth=true" "${PASSWORD}" "${USERNAME}" >> .npmrc
+printf "registry=https://purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:_password=%s\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:username=%s\n//purecloud.artifactoryonline.com/purecloud/api/npm/inin-internal-npm/:always-auth=true" "$PASSWORD" "$USERNAME" >> .npmrc
+
+cat .npmrc
