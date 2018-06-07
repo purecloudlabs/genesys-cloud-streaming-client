6c8718f24747ef8b9d9c180f190d191743fc5b0c
Xander Dumaine <xander.dumaine@inin.com>
2018-06-07 14:06:05 -0400

    remove npm utils script from build, since it's duplicated in jenkins job

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,5 @@
 DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Check out the latest npm-utils
-rm -rf ./npm-utils && git clone --depth=1 git@bitbucket.org:inindca/npm-utils.git ./npm-utils
-
-# Set up node with the provided version and generate a .npmrc file for our private npm repo
-source ./npm-utils/scripts/jenkins-pre-build.sh 8.11.2
-
 pushd ${DIR}/..
 ./node_modules/.bin/webpack
 popd
