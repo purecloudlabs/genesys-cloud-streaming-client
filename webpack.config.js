6ee14bbb5031febf5a73cf3fb3e0229783ed4c53
Xander Dumaine <xander.dumaine@genesys.com>
2018-07-10 10:30:34 -0400

    [FS-386] Do not exclude node modules from babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,6 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['env']
