761703cae0b7a016dd267f3153926c268222ff31
Xander Dumaine <xander.dumaine@inin.com>
2018-07-25 10:05:04 -0400

    enabled production build mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/client.js',
-  mode: 'development',
+  mode: 'production',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'web'),
