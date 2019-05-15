73e3e965317cea6cee8a490cd2f6692f4fc0ae05
Xander Dumaine <xander.dumaine@genesys.com>
2019-05-15 15:24:59 -0400

    upgrade stanzaio

--- src/stanzaio-light.js
+++ src/stanzaio-light.js
@@ -1,7 +1,12 @@
 'use strict';
 
 import { JID } from 'xmpp-jid';
-import Client from 'stanza.io/lib/client';
+import Client from 'stanza/client';
+import Disco from 'stanza/plugins/disco';
+import Extdisco from 'stanza/plugins/extdisco';
+import Logging from 'stanza/plugins/logging';
+import Pubsub from 'stanza/plugins/pubsub';
+import Ping from 'stanza/plugins/ping';
 
 exports.JID = JID;
 exports.Client = Client;
@@ -9,15 +14,12 @@ exports.Client = Client;
 exports.createClient = function (opts) {
   var client = new exports.Client(opts);
   [
-    require('stanza.io/lib/plugins/disco'), // must be first
+    Disco, // must be first
 
-    // TODO: figure out which of these we don't need.
-    require('stanza.io/lib/plugins/extdisco'),
-    require('stanza.io/lib/plugins/hashes'),
-    require('stanza.io/lib/plugins/json'),
-    require('stanza.io/lib/plugins/logging'),
-    require('stanza.io/lib/plugins/ping'),
-    require('stanza.io/lib/plugins/pubsub')
+    Extdisco,
+    Logging,
+    Pubsub,
+    Ping
   ].map(client.use.bind(client));
 
   return client;
