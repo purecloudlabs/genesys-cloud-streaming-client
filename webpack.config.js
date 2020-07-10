28cafe192ea94cc2a9e5083ba111aad147fb1cdd
Garrett Jensen <garrett.jensen@genesys.com>
2020-07-10 13:06:41 -0600

    [DEVCTR-180] rebrand to genesys-cloud

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename,
-      library: 'PureCloudStreamingClient',
+      library: 'GenesysCloudStreamingClient',
       libraryTarget: 'umd',
       libraryExport: 'default'
     },
