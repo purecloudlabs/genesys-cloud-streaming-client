1cf5b49ee07fc3bc27e77c5459b49320d6c6ce2f
David Houseknecht <david.j.houseknecht@gmail.com>
2020-07-31 09:24:25 -0400

    PCM-1390 pointed package.main to built webpack node target. removed node_modules from node bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
 const WebpackAutoInject = require('webpack-auto-inject-version');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = (env) => {
   const minimize = env && env.production;
   const cdn = env && env.cdn;
 
   let filename = 'streaming-client';
+  let target;
+  let libraryTarget;
+  let mode;
+  let externals = [];
   let babelExcludes = [];
+  let babelOptions;
 
   /* if building for the cdn */
   if (cdn) {
+    filename += '.bundle';
+    target = 'web';
+    libraryTarget = 'umd';
+
     /*
       this is so babel doesn't try to polyfill/transpile core-js (which is the polyfill)
         and the build tools.
@@ -21,27 +31,72 @@ module.exports = (env) => {
       /\bwebpack\/buildin\b/
     ];
 
-    filename += '.bundle';
+    babelOptions = {
+      sourceType: 'unambiguous',
+      ignore: [/\/core-js/],
+      presets: [
+        ['@babel/preset-env',
+          {
+            debug: false, /* set to `true` if you start banging your head against the wall */
+            targets: [
+              'last 2 versions',
+              '> 5%',
+              'IE 11',
+              'not dead'
+            ]
+          }
+        ]
+      ],
+      plugins: [
+        ['@babel/plugin-transform-runtime', {
+          corejs: 3
+        }]
+      ]
+    };
   } else {
-    /* if we are building for 'module', don't polyfill/transpile any dependencies */
-    babelExcludes = [/node_modules/];
+    /* we are building for node */
+    target = 'node';
+    libraryTarget = 'commonjs';
+
+    const modulesToBundle = [
+      'stanza',
+      'xmpp-jid',
+      'whatwg-fetch',
+      'genesys-cloud-streaming-client-webrtc-sessions'
+    ];
+    /* we don't want to bundle node_modules (except the ones that are es6) */
+    externals.push(nodeExternals({
+      allowlist: modulesToBundle
+    }));
+
+    /* if we are building for 'module', don't polyfill/transpile most dependencies */
+    babelExcludes = [
+      new RegExp(`/node_modules/(?!${modulesToBundle.join('|')}/)`)
+    ];
+
+    babelOptions = {
+      sourceType: 'module',
+      presets: ['@babel/preset-env']
+    };
   }
 
   filename += minimize ? '.min.js' : '.js';
+  mode = minimize ? 'production' : 'development';
 
   return {
-    target: cdn ? 'web' : 'node',
+    target,
     entry: './src/client.js',
-    mode: minimize ? 'production' : 'development',
+    mode,
     optimization: {
       minimize
     },
+    externals,
     devtool: minimize ? 'source-map' : '',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename,
       library: 'GenesysCloudStreamingClient',
-      libraryTarget: 'umd',
+      libraryTarget,
       libraryExport: 'default'
     },
     plugins: [
@@ -61,7 +116,8 @@ module.exports = (env) => {
         {
           test: /\.(c|m)?js$/,
           loader: 'babel-loader',
-          exclude: babelExcludes
+          exclude: babelExcludes,
+          options: babelOptions
         }
       ]
     }
