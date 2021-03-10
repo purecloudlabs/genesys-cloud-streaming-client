8445e833ce7db07f9c2da2c21b85e5ef381ed205
David Houseknecht <david.j.houseknecht@gmail.com>
2021-03-10 12:21:09 -0500

    PCM-1552 reworked ping logic to avoid chrome 88 timer throttling

--- src/ping.ts
+++ src/ping.ts
@@ -15,53 +15,51 @@ export class Ping {
   private pingInterval: number;
   private failedPingsBeforeDisconnect: number;
   private numberOfFailedPings: number;
-  private pingIntervalId: any;
+  private nextPingTimeoutId: any;
 
   constructor (private client: Client, private options: PingOptions = {}) {
     this.pingInterval = options.pingInterval || DEFAULT_PING_INTERVAL;
     this.failedPingsBeforeDisconnect = options.failedPingsBeforeDisconnect || DEFAULT_MAXIMUM_FAILED_PINGS_BEFORE_DISCONNECT;
     this.numberOfFailedPings = 0;
-    this.pingIntervalId = null;
+    this.nextPingTimeoutId = null;
+  }
+
+  start () {
+    if (!this.nextPingTimeoutId) {
+      this.queueNextPing();
+    }
+  }
+
+  stop () {
+    clearTimeout(this.nextPingTimeoutId);
+    this.nextPingTimeoutId = null;
+    this.numberOfFailedPings = 0;
   }
 
   private async performPing (): Promise<void> {
-    const intervalIdAtStart = this.pingIntervalId;
-
     try {
       await this.client._stanzaio.ping(this.options.jid);
       this.numberOfFailedPings = 0;
-
+      this.queueNextPing();
     } catch (err) {
       const info = {
         channelId: this.client.config.channelId,
         jid: this.client._stanzaio.jid
       };
+      this.client.logger.warn('Missed a ping.', Object.assign({ error: err }, info));
 
-      /* need to check this because we could have already stopped the pinging. In which case, we don't want to count the missed ping */
-      if (intervalIdAtStart !== this.pingIntervalId) {
-        return this.client.logger.warn('Disregarding a missed ping for an old interval', Object.assign({ error: err }, info));
-      }
-
-      this.client.logger.warn('Missed a ping.', Object.assign({ error: err }, info, { intervalIdAtStart, pingIntervalId: this.pingIntervalId }));
+      /* if we have reached max number of missed pings, disconnect */
       if (++this.numberOfFailedPings > this.failedPingsBeforeDisconnect) {
         this.stop();
         this.client.logger.error('Missed too many pings, disconnecting', Object.assign({ numberOfFailedPings: this.numberOfFailedPings }, info));
         this.client._stanzaio.sendStreamError({ text: 'too many missed pongs', condition: 'connection-timeout' });
+      } else {
+        this.queueNextPing();
       }
     }
   }
 
-  start () {
-    if (this.pingIntervalId === null) {
-      this.pingIntervalId = setInterval(this.performPing.bind(this), this.pingInterval);
-    }
-  }
-
-  stop () {
-    if (this.pingIntervalId !== null) {
-      clearInterval(this.pingIntervalId);
-      this.pingIntervalId = null;
-      this.numberOfFailedPings = 0;
-    }
+  private queueNextPing () {
+    this.nextPingTimeoutId = setTimeout(this.performPing.bind(this), this.pingInterval);
   }
 }
