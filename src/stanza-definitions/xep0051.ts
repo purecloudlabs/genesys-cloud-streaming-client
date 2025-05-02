a77747ea30dd133269c0d9fc42eb38044e9b05f9
Jon Hjelle <jon.hjelle@genesys.com>
2025-05-02 16:26:49 -0700

    [STREAM-85] Add JXT definition for connection transfer XEP

---
+++ src/stanza-definitions/xep0051.ts
@@ -0,0 +1,45 @@
+import { Stanzas } from 'stanza';
+import { childText, DefinitionOptions } from 'stanza/jxt';
+
+// XEP-0051: Connection Transfer
+// Source: https://xmpp.org/extensions/xep-0051.html
+// Version: 0.2.1 (2022-03-08)
+// ---------------------------
+// We don't currently use domain or server
+
+const NS_CONNECTION_TRANSFER = 'urn:xmpp:cxfr';
+
+declare module 'stanza/protocol' {
+  export interface IQPayload {
+    query?: ConnectionTransfer;
+  }
+
+  export interface AgentEvents {
+    /* tslint:disable-next-line no-unnecessary-qualifier */
+    'iq:set:connectionTransfer': Stanzas.ReceivedIQ & { query: ConnectionTransfer };
+  }
+}
+
+export interface ConnectionTransfer {
+  domain?: string;
+  server?: string;
+}
+
+/*
+  <iq xmlns="jabber:client" id="<someId>" to="<toJID>" type="set">
+    <query xmlns="urn:xmpp:cxfr">
+      <domain>jabber.org</domain>
+      <server>123.123.123.122</server>
+    </query>
+  </iq>
+*/
+export const connectionTransfer: DefinitionOptions = {
+  path: 'iq.query',
+  aliases: ['iq.connectionTransfer'],
+  namespace: NS_CONNECTION_TRANSFER,
+  element: 'query',
+  fields: {
+    domain: childText(null, 'domain'),
+    server: childText(null, 'server')
+  }
+};
