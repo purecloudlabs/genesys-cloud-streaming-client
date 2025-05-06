3e357549a533ea034e3ab06fb6784322bf1ce395
Jon Hjelle <jon.hjelle@genesys.com>
2025-05-06 09:32:34 -0700

    [STREAM-85] Add class to handle connection transfer IQs

---
+++ src/connection-transfer.ts
@@ -0,0 +1,18 @@
+import { IQ } from 'stanza/protocol';
+
+import { Client } from './client';
+import { connectionTransfer } from './stanza-definitions/xep0051';
+import { NamedAgent } from './types/named-agent';
+
+export class ConnectionTransfer {
+  constructor (private client: Client, private stanzaInstance: NamedAgent) {
+    stanzaInstance.stanzas.define(connectionTransfer);
+
+    // Hawk maps `v2.system.socket_closing` to XEP-0051 Connection Transfer
+    // The docs says we have up to one minute to disconnect and connect a new WebSocket, so we should be proactive in reconnecting.
+    stanzaInstance.on('iq:set:connectionTransfer', (iq: IQ) => {
+      void this.client.disconnect();
+      void this.client.connect({ keepTryingOnFailure: true });
+    });
+  }
+}
