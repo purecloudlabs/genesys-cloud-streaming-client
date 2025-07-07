dff0f0e9ca42f68ff23b9babbafc03fe9b08db73
Jon Hjelle <jon.hjelle@genesys.com>
2025-07-07 11:10:44 -0700

    [STREAM-85] Include the `disconnected` event as part of the test

--- test/unit/connection-transfer.test.ts
+++ test/unit/connection-transfer.test.ts
@@ -2,8 +2,23 @@ import EventEmitter from "events";
 import { Stanzas } from 'stanza';
 import { v4 as uuidv4 } from "uuid";
 
-import { NamedAgent } from "../../src/types/named-agent";
 import { ConnectionTransfer } from "../../src/connection-transfer";
+import Client from "../../src";
+import { NamedAgent } from "../../src/types/named-agent";
+
+function getFakeClient (): Client {
+  const instance = new EventEmitter();
+  return Object.assign(
+    instance,
+    {
+      connect: jest.fn(),
+      disconnect: jest.fn(),
+      logger: {
+        warn: jest.fn()
+      }
+    }
+  ) as unknown as Client;
+}
 
 function getFakeStanzaClient (): NamedAgent {
   const instance = new EventEmitter();
@@ -20,14 +35,7 @@ function getFakeStanzaClient (): NamedAgent {
 describe('ConnectionTransfer', () => {
   describe('constructor()', () => {
     it('disconnects and reconnects on the iq:set:connectionTransfer event', () => {
-      const fakeClient = {
-        connect: jest.fn(),
-        disconnect: jest.fn(),
-        emit: jest.fn(),
-        logger: {
-          warn: jest.fn()
-        }
-      };
+      const fakeClient = getFakeClient();
       const fakeStanza = getFakeStanzaClient();
       const iq = {
         lang: '',
@@ -38,10 +46,16 @@ describe('ConnectionTransfer', () => {
 
       const connectionTransfer = new ConnectionTransfer(fakeClient as any, fakeStanza);
 
+      fakeClient.on('disconnected', () => {
+        expect(true).toBeTruthy();
+      });
+
       fakeStanza.emit('iq:set:connectionTransfer', iq);
 
       expect(fakeClient.disconnect).toHaveBeenCalled();
       expect(fakeClient.connect).toHaveBeenCalled();
+
+      expect.assertions(3);
     });
   });
 });
