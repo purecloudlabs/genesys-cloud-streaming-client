66f8f1f573bc101e47986faf2923ef464b074d3b
Jon Hjelle <jon.hjelle@genesys.com>
2026-02-03 12:08:48 -0800

    [STREAM-1179] Grab the connectionId from stanza's stream

--- src/alerting-leader.ts
+++ src/alerting-leader.ts
@@ -3,12 +3,12 @@ import { Client } from './client';
 import { NamedAgent } from './types/named-agent';
 
 export class AlertingLeaderExtension implements StreamingClientExtension {
-  private stanzaInstance?: NamedAgent;
+  private connectionId?: string;
 
   constructor (private client: Client) { }
 
   handleStanzaInstanceChange (stanzaInstance: NamedAgent) {
-    this.stanzaInstance = stanzaInstance;
+    this.connectionId = stanzaInstance.transport?.stream?.id;
   }
 
   get expose (): AlertingLeaderApi {
